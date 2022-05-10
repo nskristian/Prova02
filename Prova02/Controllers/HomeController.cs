517b0b4fcb91db50a55d359f978978347cb353f1
nskristian <ns.kristian@hotmail.com>
2022-05-10 13:17:20 -0300

    Prova02

---
+++ Prova02/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prova02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prova02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(StringReverseModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var texto = model.Texto;
+                    var textoReverso = new string(texto.Reverse().ToArray());
+
+                    TempData["texto"] = textoReverso;
+
+                    if(texto.ToLower() == textoReverso.ToLower())
+                    {
+                        TempData["MensagemSucesso"] = "É um palíndromo!";
+                    }
+                }
+                catch (Exception e)
+                {
+                    TempData["MensagemErro"] = "Erro: " + e.Message;
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Prova02/Models/StringReverseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prova02.Models
+{
+    public class StringReverseModel
+    {
+        [MinLength(3, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor informe o texto que será invertido")]
+        public string Texto { get; set; }
+    }
+}
